bfcdcfeb87d6af69f340b7dba81717946133a02c
marciovin <marcioviniciusfilho760@gmail.com>
2024-04-22 11:23:49 -0300

    terminando de animar o circulo

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -5,7 +5,9 @@ import {
   Skia,
   useValue,
   runTiming,
-  BlurMask
+  BlurMask,
+  Circle,
+  Easing
 } from "@shopify/react-native-skia"
 
 import { styles } from './styles';
@@ -22,8 +24,10 @@ const CHECK_STROKE = 2
 
 export function Option({ checked, title, ...rest }: Props) {
   const percentage = useValue(0)
+  const Cicle = useValue(0)
 
   const Radius = (CHECK_SIZE - CHECK_STROKE) / 2;
+  const CENTER_CICLE = Radius / 2;
 
   const path = Skia.Path.Make();
   path.addCircle(CHECK_SIZE, CHECK_SIZE, Radius)
@@ -31,8 +35,10 @@ export function Option({ checked, title, ...rest }: Props) {
   useEffect(() => {
     if (checked) {
       runTiming(percentage, 1, { duration: 700 });
+      runTiming(Cicle, CENTER_CICLE, { easing: Easing.bounce });
     } else {
       runTiming(percentage, 0, { duration: 700 });
+      runTiming(Cicle, 0, { duration: 300 });
     }
 
   }, [checked])
@@ -68,6 +74,16 @@ export function Option({ checked, title, ...rest }: Props) {
         >
           <BlurMask blur={1} style={'solid'} />
         </Path>
+
+        <Circle
+          cx={CHECK_SIZE}
+          cy={CHECK_SIZE}
+          r={Cicle}
+          color={THEME.COLORS.BRAND_LIGHT}
+        >
+          <BlurMask blur={4} style={'inner'} />
+        </Circle>
+
       </Canvas>
     </TouchableOpacity>
   );
